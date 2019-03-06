b3e30c6492104b3154bb8eef110bab1362bbaa95
pasha <pasharudenko@ukr.net>
2019-03-06 13:43:35 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <div class="col-xs-12">
+           <transition name="slide" mode="out-in">
+               <router-view></router-view>
+           </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    export default {
+        components: {
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px;
+    }
+
+    .slide-enter-active {
+        animation: side-in 200ms ease-out ;
+    }
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out ;
+    }
+
+
+    @keyframes side-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+    }
+</style>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
+</template>
+
+<script>
+    import Stock from './Stock.vue';
+    export default {
+        computed: {
+          stocks() {
+              return this.$store.getters.stocks;
+          }
+        },
+        components: {
+            appStock: Stock
+        },
+        created() {
+            this.$store.dispatch('initStocks');
+        }
+    }
+</script>
